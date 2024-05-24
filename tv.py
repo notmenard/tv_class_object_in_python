cd4c0a128e06996e926b4cbcdb95c08c1f0a1c0d
Menard Sabugo <menardsabugo@gmail.com>
2024-05-24 15:16:41 +0800

    Pseudocode for the tv.py file

---
+++ tv.py
@@ -0,0 +1,12 @@
+# Class representing the television
+# Initialize the TV object with default values for channel, volume, and is_on
+# Turn on the TV
+# Turn off the TV
+# Set the channel of the TV
+# Set the volume of the TV
+# Get the current channel of the TV
+# Get the current volume of the TV
+# Increase the channel by 1
+# Decrease the channel by 1
+# Increase the volume by 1
+# Decrease the volume by 1
