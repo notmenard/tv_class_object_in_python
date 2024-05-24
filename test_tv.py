193e1dfd08ba9de5757e8538eb7ffc6acfacf422
Menard Sabugo <menardsabugo@gmail.com>
2024-05-24 18:29:19 +0800

    Creating a Tv 2 Object

--- test_tv.py
+++ test_tv.py
@@ -8,6 +8,7 @@ def test_tv():
     # Create a TV object tv1
     tv1 = TV(channel=30, volume=3, is_on=True)
     # Create a TV object tv2
+    tv2 = TV(channel=3, volume=2, is_on=True)
 # Print the channel and volume level of tv1
 # Print the channel and volume level of tv2
 # Run the tests if this script is executed directly
