c9d0e2d77e83e64d86cb631849546c3d4f761272
Menard Sabugo <menardsabugo@gmail.com>
2024-05-24 15:24:01 +0800

    Initializing the TV object with default values

--- tv.py
+++ tv.py
@@ -2,6 +2,11 @@
 class TV:
     # Initialize the TV object with default values for channel, volume, and is_on
     def __init__(self, channel=1, volume=1, is_on=False):
+        self.channel = channel
+        self.volume = volume
+        self.is_on = is_on
+        self.channels = list(range(1, 31))
+
 # Turn on the TV
 # Turn off the TV
 # Set the channel of the TV
