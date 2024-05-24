19bf8149fbcf50968ac925129b01c1b2a94da85f
Menard Sabugo <menardsabugo@gmail.com>
2024-05-24 18:23:58 +0800

    Pseudocode for the test_tv.py

---
+++ test_tv.py
@@ -0,0 +1,7 @@
+# Import the TV class from the TV module
+# Define a function to test the TV class
+# Create a TV object tv1
+# Create a TV object tv2
+# Print the channel and volume level of tv1
+# Print the channel and volume level of tv2
+# Run the tests if this script is executed directly
