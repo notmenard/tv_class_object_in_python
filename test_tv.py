405b69ab063232fbe03dd60391f38fdd93d39a6e
Menard Sabugo <menardsabugo@gmail.com>
2024-05-24 18:33:26 +0800

    Printing the channel and volume level of Tv 2

--- test_tv.py
+++ test_tv.py
@@ -13,4 +13,5 @@ def test_tv():
     # Print the channel and volume level of tv1
     print("\033[92mTv 1's channel is ", {tv1.get_channel()}, " and volume level is ", {tv1.get_volume()}, "\033[0m")
     # Print the channel and volume level of tv2
+    print("\033[92mTv 2's channel is ", {tv2.get_channel()}, " and volume level is ", {tv2.get_volume()}, "\033[0m")
 # Run the tests if this script is executed directly
